f4a61d58999438ea412dd7f7b3b628d3dceafccc
Rodrigo <rodvalenciaa@gmail.com>
2022-02-10 15:18:17 -0800

    name prompt

--- assets/js/game.js
+++ assets/js/game.js
@@ -86,15 +86,13 @@ var startGame = function() {
         if (playerInfo.health > 0) {
             // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
             window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
+            //debugger;
 
             // pick new enemy to fight based on the index of the enemy.names array
             var pickedEnemyObj = enemyInfo[i];
 
             // reset enemy.health before starting new fight
             pickedEnemyObj.health = randomNumber(40, 60);
-
-            // use debugger to pause script from running and check what's going on at that moment in the code
-            // debugger;
 
             // pass the pickedEnemy.name variable's value into the fight function, where it will assume the value fo the enemy.name parameter
             fight(pickedEnemyObj);
@@ -177,8 +175,21 @@ var shop = function(){
 
 };
 
+// function to set name
+var getPlayerName = function() {
+    var name = "";
+
+    while (name === "" || name === null) {
+        name - prompt("What is your robot's name?");
+    }
+
+    console.log("Your robot's name is " + name);
+    return name;
+};
+
+/* GAME INFORMATION / VARIABLES */
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
